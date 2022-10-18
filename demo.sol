be261e5ae0986ccd06ba1b55236476834972a1c8
Remix Workspace <>
2022-10-18 13:01:43 +0530

    concept of pure and view

--- demo.sol
+++ demo.sol
@@ -6,16 +6,24 @@ contract demo{
     // string public sentence="helloooooo world";
     uint public age = 10; // is a state variable//declared contract level
     string public name;
+     uint256 public e = 5;
+
     constructor(){
 
         age = 100;
     }
-
+    function increment() public{
+        age=age+1;
+    }
     function setter() public{
 
         age = 20;
 
     }
 
+   function variableE() public returns (uint256) {
+        e = e + 1;
+        return e;
+    }
 
 }
