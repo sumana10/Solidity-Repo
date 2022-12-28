f2a5622e7714bc784943e047f5a9fe8153dfe5a6
Sumana <63727050+sumana10@users.noreply.github.com>
2022-12-28 15:58:36 +0530

    Create abiFunction.sol

---
+++ abiFunction.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.6;
+
+
+contract EncodeDecode{
+
+    function encode(string memory _str1, uint _int, string memory _str2) public pure returns(bytes memory){
+
+        
+            return (abi.encode(_str1, _int, _str2));
+
+    }
+
+    function decode1(bytes memory data) public pure returns(string memory _str1,uint _int,string memory _str2){
+       
+        (_str1,_int,_str2)=abi.decode(data,(string,uint,string));
+       
+    }
+
+}
